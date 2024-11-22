36a697599ea005503fa8875de31d6840f3227562
Tifani <tifani23ti@mahasiswa.pcr.ac.id>
2024-11-22 11:09:36 +0700

    Ubah judul

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Dapur Marani GG</title>
+        <title>Dapur Marani GGg</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
